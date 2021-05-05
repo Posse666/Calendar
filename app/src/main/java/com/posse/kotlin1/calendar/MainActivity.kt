e47c5c99ab7848777b3538b37610d9b97afdc007
Posse <posse666666@gmail.com>
2021-05-05 23:43:14 +0300

    Convert to Kotlin. Added calendar and settings fragments.

---
+++ app/src/main/java/com/posse/kotlin1/calendar/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.posse.kotlin1.calendar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.posse.kotlin1.calendar.ui.SectionsPagerAdapter
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val sectionsPagerAdapter = SectionsPagerAdapter(this)
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = sectionsPagerAdapter.tabTitles[position]
+            viewPager.setCurrentItem(tab.position, true)
+        }.attach()
+
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
