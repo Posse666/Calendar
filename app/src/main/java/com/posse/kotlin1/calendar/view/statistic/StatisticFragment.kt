be94f7ee4028b0c4c70bb58d75c378bda905a1f1
Posse <posse66666@gmail.com>
2021-07-29 10:20:36 +0300

    Added font, added spannable statistic text

--- app/src/main/java/com/posse/kotlin1/calendar/view/statistic/StatisticFragment.kt
+++ app/src/main/java/com/posse/kotlin1/calendar/view/statistic/StatisticFragment.kt
@@ -1,14 +1,18 @@
 package com.posse.kotlin1.calendar.view.statistic
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.google.android.material.card.MaterialCardView
 import com.posse.kotlin1.calendar.R
 import com.posse.kotlin1.calendar.databinding.FragmentStatisticBinding
-import com.posse.kotlin1.calendar.utils.putText
 import com.posse.kotlin1.calendar.viewModel.CalendarViewModel
 import com.posse.kotlin1.calendar.viewModel.STATISTIC
 import java.time.LocalDate
@@ -40,43 +44,70 @@ class StatisticFragment : Fragment() {
 
     private fun updateTotal(stats: Map<STATISTIC, Set<LocalDate>>) {
         val total = stats[STATISTIC.DRINK_DAYS_THIS_YEAR]?.size ?: 0
-        binding.totalYearStats.caption.putText(getString(R.string.in_this_year_you_drank))
-        binding.totalYearStats.firstStat.putText("$total ")
-        binding.totalYearStats.description.putText(
+        putStatistic(
+            total,
+            stats[STATISTIC.DAYS_THIS_YEAR]?.size.toString(),
+            getString(R.string.in_this_year_you_drank),
             resources.getQuantityString(
                 R.plurals.days,
                 total
-            ) + getString(R.string.of)
+            ) + getString(R.string.of),
+            null,
+            binding.totalYearStats,
+            null
         )
-        binding.totalYearStats.secondStat.putText(stats[STATISTIC.DAYS_THIS_YEAR]?.size ?: 0)
     }
 
     private fun updateMarathonThisYear(stats: Set<LocalDate>?) {
         val thisYear = stats?.size ?: 0
-        binding.longestDrinkThisYear.caption.putText(getString(R.string.longest_drink_marathon_in_this_year))
-        binding.longestDrinkThisYear.firstStat.putText("$thisYear ")
-        binding.longestDrinkThisYear.description.putText(
+        putStatistic(
+            thisYear,
+            "",
+            getString(R.string.longest_drink_marathon_in_this_year),
             resources.getQuantityString(
                 R.plurals.days,
                 thisYear
-            )
+            ),
+            stats,
+            binding.longestDrinkThisYear,
+            binding.cardThisYear
         )
-        binding.cardThisYear.setOnClickListener {
-            listener?.cardStatsPressed(stats?.minOrNull() ?: LocalDate.now())
-        }
     }
 
     private fun updateMarathonAllTime(stats: Set<LocalDate>?) {
         val allTime = stats?.size ?: 0
-        binding.longestDrinkAllTime.caption.putText(getString(R.string.longest_drink_marathon_all_time))
-        binding.longestDrinkAllTime.firstStat.putText("$allTime ")
-        binding.longestDrinkAllTime.description.putText(
+        putStatistic(
+            allTime,
+            "",
+            getString(R.string.longest_drink_marathon_all_time),
             resources.getQuantityString(
                 R.plurals.days,
                 allTime
-            )
+            ),
+            stats,
+            binding.longestDrinkAllTime,
+            binding.cardAllTime
         )
-        binding.cardAllTime.setOnClickListener {
+    }
+
+    private fun putStatistic(
+        firstStat: Int,
+        secondStat: String,
+        description: String,
+        plurals: String,
+        stats: Set<LocalDate>?,
+        textView: TextView,
+        cardView: MaterialCardView?
+    ) {
+        val text = "$description$firstStat $plurals $secondStat"
+        val spannable = SpannableString(text)
+        spannable.setSpan(
+            ForegroundColorSpan(resources.getColor(R.color.fillColor, null)),
+            description.length, description.length + firstStat.toString().length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        textView.text = spannable
+        cardView?.setOnClickListener {
             listener?.cardStatsPressed(stats?.minOrNull() ?: LocalDate.now())
         }
     }
